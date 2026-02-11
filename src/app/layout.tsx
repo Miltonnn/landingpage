c4d83fa558d80d71e35ab2048fa8a9acabb39b54
Miltonnn <juninhoeufrasio2017@hotmail.com>
2026-02-11 11:17:01 -0300

    REFACTOR: Nome site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DOGGY",
+  title: "Pet Shop Dev",
   description: "Seu pet merece cuidado, carinho e atenção especial",
 };
 
